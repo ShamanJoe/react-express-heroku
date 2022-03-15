9557b84f34a59c5dd110d6718eadaf1256defd4a
ShamanJoe <jonathansolheim@outlook.com>
2022-03-15 18:55:42 +0100

    Retry #1

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`started on https://localhost: ${server.address().port}`)
+    console.log(`started on https://localhost:${server.address().port}`)
 })
